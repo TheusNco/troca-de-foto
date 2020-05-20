e376cd2165b79bc3b6e5b17b8a183a9381af82fc
Trincazul <endriw_villa@hotmail.com>
2020-05-20 11:58:00 -0300

    tentando

---
+++ app/src/main/java/com/example/trocafto/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.trocafto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void clickCavalo(View view){
+
+    }
+}
