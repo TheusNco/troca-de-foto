7bb8b04b801fc8bdc1c0583e975b2978b3e7b19f
Trincazul <endriw_villa@hotmail.com>
2020-05-22 14:03:58 -0300

    Foi adicionado os intanciamento das ações

--- app/src/main/java/com/example/trocafto/MainActivity.java
+++ app/src/main/java/com/example/trocafto/MainActivity.java
@@ -4,15 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+        private Button cavalo;
+        private Button gato;
+        private Button galinha;
+        private Button cachorro;
+        private ImageView troca;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cavalo = (Button) findViewById(R.id.btncavalo);
+        gato = (Button) findViewById(R.id.btngato);
+        galinha = (Button) findViewById(R.id.btngalinha);
+        cachorro = (Button) findViewById(R.id.btncachorro);
+        troca = (ImageView) findViewById(R.id.imgtroca);
+
     }
 
     public void clickCavalo(View view){
