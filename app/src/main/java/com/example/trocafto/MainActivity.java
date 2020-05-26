2b41a8078a9458743f1baec289b8f5c60ea89f42
Trincazul <endriw_villa@hotmail.com>
2020-05-26 14:42:40 -0300

    Foi adicionado nos metodos a chamada de som

--- app/src/main/java/com/example/trocafto/MainActivity.java
+++ app/src/main/java/com/example/trocafto/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.trocafto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -31,14 +32,22 @@ public class MainActivity extends AppCompatActivity {
 
     public void clickCavalo(View view){
         troca.setImageResource(R.drawable.cavalo);
+        MediaPlayer mpcavalo = MediaPlayer.create(this, R.drawable.somcavalo);
+        mpcavalo.start();
     }
     public void clickGato(View view){
         troca.setImageResource(R.drawable.gato);
+        MediaPlayer mpgato = MediaPlayer.create(this, R.drawable.somgato);
+        mpgato.start();
     }
     public void clickGalinha(View view){
         troca.setImageResource(R.drawable.galinha);
+        MediaPlayer mpgalinha = MediaPlayer.create(this, R.drawable.somgalinha);
+        mpgalinha.start();
     }
     public void clickCachorro(View view){
         troca.setImageResource(R.drawable.cachorro);
+        MediaPlayer mpcachorro = MediaPlayer.create(this, R.drawable.somcachorro);
+        mpcachorro.start();
     }
 }
